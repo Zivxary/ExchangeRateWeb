3217f4760cf2c96f81e65fe6f09941327f17fddd
Zivxary <childbrozoo8@gmail.com>
2018-09-22 22:40:21 +0800

    負責從session取值

---
+++ src/pers/zivxary/first/web/utils/SessionUtil.java
@@ -0,0 +1,45 @@
+package pers.zivxary.first.web.utils;
+
+import javax.servlet.http.HttpSession;
+
+public class SessionUtil {
+
+    private IDateUtil dateUtil = new DateUtil();
+    private HttpSession session;
+
+    public SessionUtil(HttpSession session) {
+	this.session = session;
+    }
+
+    /**
+     * 查看session.getAttribute(key)是否有值。有就使用，沒有就取當前日期
+     * 
+     * @param key 鍵值
+     * @return 返回取到的值，沒有就返回當前日期
+     */
+    public String getDayFromSession(String key) {
+	return getFormSession(key, dateUtil.getNowDay());
+    }
+
+    /**
+     * 查看session.getAttribute(key)是否有值。有就使用，沒有就取當前時間
+     * 
+     * @param key 鍵值
+     * @return 返回取到的值，沒有就返回當前時間
+     */
+    public String getTimeFromSession(String key) {
+	return getFormSession(key, dateUtil.getNowTime());
+    }
+
+    /**
+     * 從session.getAttribute 取key，如果有就返回，沒有就返回defaultVal
+     * 
+     * @param key        鍵值
+     * @param defaultVal 沒有值時的返回值
+     * @return 返回取到的值，沒有就返回defaultVal
+     */
+    public String getFormSession(String key, String defaultVal) {
+	String s = (String) session.getAttribute(key);
+	return (s == null) ? defaultVal : s;
+    }
+}
